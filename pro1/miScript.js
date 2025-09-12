b4cc0b0d5d5d4ffd92da5b61b99e70ed24c7bc60
Arman <armkeyahm2@alu.edu.gva.es>
2025-09-12 19:51:02 +0200

    1.1

--- pro1/miScript.js
+++ pro1/miScript.js
@@ -1,8 +1,12 @@
 let hola2 = document.getElementById("hola2");
 let big_shot = prompt("Hola clase, ¿cómo se llama este Big Shot?");
+let imagen = document.getElementById("imagen");
+
 console.log(big_shot);
-if(!big_shot){
+if(!big_shot || big_shot === " "){
     hola2.textContent = "Seguimos sin Big Shot";
+    console.log(big_shot.split(" "));
+    imagen.classList.remove('img_hover');
 }
 else {
     hola2.textContent = big_shot + " es nuestro Big Shot";
