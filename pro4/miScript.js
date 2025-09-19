971bcf9027bf45b159fe8782877e0e3808e4b97a
Arman <armkeyahm2@alu.edu.gva.es>
2025-09-19 19:52:26 +0200

    1.4

--- pro4/miScript.js
+++ pro4/miScript.js
@@ -1,16 +1,22 @@
-
-
 function adivinaNumero(){
-    do{
-        let bucle = true
-        let num_aleatorio = Math.floor(Math.random() * (100 - 1) + 1);
-        let prompt = prompt("Adivina un número del 1 al 100");
-        if(num_aleatorio == prompt){
+        let bucle = true;
+
+    do{    
+        let num_aleatorio = Math.floor(Math.random() * 100 + 1);
+        console.log(num_aleatorio);
+        let num_elegido = prompt("Adivina un número del 1 al 100");
+        console.log(num_elegido);
+        if(num_aleatorio == num_elegido){
             bucle = false;
+        }
+        else if(num_aleatorio > num_elegido){
+            alert(`Te has equivocado el número era más grande`);
+        }
+        else if(num_aleatorio > num_elegido){
+            alert(`Te has equivocado el número era más pequeño`);
         }
     }
     while(bucle)
 }
-
 
 adivinaNumero();
